01d8c58b39ddab34c5246c7e630dd9cc18346250
Shadow <Nayem58@users.noreply.github.com>
2023-05-28 10:59:18 +0600

    simple counter: increase, decrease, and set to zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,28 @@
 import "./App.css";
 import { UserCard } from "./components/UserCard";
+import { useState } from "react";
 
 function App() {
-  const empNames = [
-    {
-      name: "Solaiman",
-      title: "Front-End",
-      photo:
-        "https://www.americanbestit.com/images/meet-the-team/10744-mohammed-solaiman.jpg",
-    },
-    {
-      name: "Kabir",
-      title: "Webmaster",
-      photo:
-        "https://www.americanbestit.com/images/meet-the-team/37416-Kabir.jpg",
-    },
-    {
-      name: "Romman",
-      title: "Front-End",
-      photo:
-        "https://www.americanbestit.com/images/meet-the-team/85462-kkr_p.jpeg",
-    },
-    {
-      name: "Tofazzal",
-      title: "Webmaster",
-      photo:
-        "https://www.americanbestit.com/images/meet-the-team/81899-tofazzal-al-hoque-abit-profile-photo.jpg",
-    },
-    {
-      name: "Jahangir",
-      title: "Sales Manager",
-      photo:
-        "https://www.americanbestit.com/images/meet-the-team/38589-jahangir-alam.jpg",
-    },
-    {
-      name: "Marof Billah",
-      title: "Creative Designer",
-      photo:
-        "https://www.americanbestit.com/images/meet-the-team/13858-marof-billah.jpg",
-    },
-  ];
+  const [count, setCount] = useState(0);
+
+  const handleSetIncreaseCount = () => {
+    setCount(count + 1);
+  };
+  const handleSetDecreaseCount = () => {
+    setCount(count - 1);
+  };
+  const handleSetToZero = () => {
+    setCount(0);
+  };
 
   return (
     <div className="py-50px">
       <div className="container">
-        <h1>Employee Cards</h1>
-        <div className="cards-grid">
-          {empNames.map(
-            ({ photo, name, title }, key) =>
-              title !== "Front-End" && (
-                <UserCard
-                  key={key}
-                  imgSrc={photo}
-                  imgAlt={name}
-                  name={name}
-                  title={title}
-                />
-              )
-          )}
-        </div>
+        <h1 className="text-center">Counter</h1>
+        <div>Count: {count}</div>
+        <button onClick={handleSetIncreaseCount}>Increase</button>
+        <button onClick={handleSetDecreaseCount}>Decrease</button>
+        <button onClick={handleSetToZero}>Set to Zero</button>
       </div>
     </div>
   );
