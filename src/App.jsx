0cba5e7e73d68fd9c37ef8bbbd942a040d2810bd
Shadow <Nayem58@users.noreply.github.com>
2023-05-28 13:33:54 +0600

    created the to-do list app

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,44 @@ import { UserCard } from "./components/UserCard";
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("");
-  const [text, setText] = useState("Black");
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSetNewTask = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleSetTasks = () => {
+    setTasks([...tasks, newTask]);
+  };
 
   return (
     <div className="py-50px text-center">
       <div className="container">
-        <h1>Text Color Change</h1>
-        <p style={{ color: color }}>{text}</p>
-        <button
-          onClick={() => {
-            setColor(color === "slateblue" ? "black" : "slateblue");
-            setText(color === "slateblue" ? "Black" : "Slate Blue");
-          }}
-        >
-          Change Color
-        </button>
+        <div className="todoCard">
+          <h1>To-Do List</h1>
+          <div className="mt-10px">
+            <input type="text" onChange={handleSetNewTask} />
+            <button onClick={handleSetTasks}>Add</button>
+          </div>
+          <ul className="mt-10px">
+            {tasks.map((task, key) => {
+              return (
+                <li key={key}>
+                  {key +
+                    1 +
+                    ". " +
+                    task.replace(/\w\S*/g, function (txt) {
+                      return (
+                        txt.charAt(0).toUpperCase() +
+                        txt.substr(1).toLowerCase()
+                      );
+                    })}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
